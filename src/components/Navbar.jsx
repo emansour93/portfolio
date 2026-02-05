f1d9b592c67796c66d5d91e05dd6ecee36ec3646
emansour93 <elie_100@hotmail.com>
2026-02-05 21:21:56 +0200

    v1.9 fix navbar close when change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,9 @@ import logo from "../assets/logos/logo-white-s.png";
 
 export default function NavbarComponent() {
   const [scrolled, setScrolled] = useState(false);
+  const [expanded, setExpanded] = useState(false);
   const location = useLocation();
+
   const isHome = location.pathname === "/";
 
   useEffect(() => {
@@ -13,6 +15,11 @@ export default function NavbarComponent() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
+  // 🔥 Close menu when route changes
+  useEffect(() => {
+    setExpanded(false);
+  }, [location.pathname]);
 
   const navbarClass = isHome
     ? scrolled
@@ -25,6 +32,7 @@ export default function NavbarComponent() {
       collapseOnSelect
       expand="md"
       fixed="top"
+      expanded={expanded}
       className={navbarClass}
       variant="light"
     >
@@ -33,22 +41,42 @@ export default function NavbarComponent() {
           <img src={logo} className="menu-logo" alt="RGM Development" />
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => setExpanded(expanded ? false : "expanded")}
+        />
+
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={NavLink} to="/">
+            <Nav.Link as={NavLink} to="/" onClick={() => setExpanded(false)}>
               Home
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/about">
+            <Nav.Link
+              as={NavLink}
+              to="/about"
+              onClick={() => setExpanded(false)}
+            >
               About Us
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/services">
+            <Nav.Link
+              as={NavLink}
+              to="/services"
+              onClick={() => setExpanded(false)}
+            >
               Services
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/projects">
+            <Nav.Link
+              as={NavLink}
+              to="/projects"
+              onClick={() => setExpanded(false)}
+            >
               Projects
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/contact">
+            <Nav.Link
+              as={NavLink}
+              to="/contact"
+              onClick={() => setExpanded(false)}
+            >
               Contact Us
             </Nav.Link>
           </Nav>
